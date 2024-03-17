2388fe75d3f75acd765d723245592b34864ed155
Bipasha <bipashagarg06@gmail.com>
2024-03-18 00:03:37 +0530

    slider for line chart

--- economics-of-asylum-countries/src/graphs/LineChart.jsx
+++ economics-of-asylum-countries/src/graphs/LineChart.jsx
@@ -1,10 +1,14 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import * as d3 from "d3";
 
 function LineChart() {
+  const [sliderValue, setSliderValue] = useState(0);
+  const [minX, setMinX] = useState(0);
+  const [maxX, setMaxX] = useState(0);
+
   useEffect(() => {
     const dataset1 = [
-      [1, 1],
+      [0, 1],
       [12, 20],
       [24, 36],
       [32, 50],
@@ -19,7 +23,15 @@ function LineChart() {
       [100, 140],
     ];
 
-    const margin = { top: 50, right: 50, bottom: 50, left: 50 };
+    var xValues = dataset1.map((d) => d[0]);
+    const minXValue = d3.min(xValues);
+    const maxXValue = d3.max(xValues);
+    setMinX(minXValue);
+    setMaxX(maxXValue);
+
+    var filteredDataset = dataset1.filter((d) => d[0] <= sliderValue);
+
+    const margin = { top: 50, right: 50, bottom: 100, left: 50 };
     const width = 500 - margin.left - margin.right;
     const height = 400 - margin.top - margin.bottom;
 
@@ -32,6 +44,10 @@ function LineChart() {
       .attr("height", height + margin.top + margin.bottom)
       .append("g")
       .attr("transform", `translate(${margin.left}, ${margin.top})`);
+
+    // Remove previous lines and circles
+    svg.selectAll(".line").remove();
+    svg.selectAll("circle").remove();
 
     svg
       .append("text")
@@ -70,8 +86,8 @@ function LineChart() {
     svg.append("g").call(d3.axisLeft(yScale));
 
     svg
-      .selectAll("dot")
-      .data(dataset1)
+      .selectAll("circle")
+      .data(filteredDataset)
       .enter()
       .append("circle")
       .attr("cx", (d) => xScale(d[0]))
@@ -87,15 +103,74 @@ function LineChart() {
 
     svg
       .append("path")
-      .datum(dataset1)
+      .datum(filteredDataset)
       .attr("class", "line")
       .attr("d", line)
       .style("fill", "none")
       .style("stroke", "#CC0000")
       .style("stroke-width", "2");
-  }, []);
 
-  return <svg />;
+    // Add legend
+    svg
+      .append("circle")
+      .attr("cx", width - 100)
+      .attr("cy", 10)
+      .attr("r", 3)
+      .style("fill", "#CC0000");
+
+    svg
+      .append("text")
+      .attr("x", width - 90)
+      .attr("y", 10)
+      .text("Data")
+      .style("font-size", "12px")
+      .attr("alignment-baseline", "middle");
+  }, [sliderValue]);
+
+  const handleSliderChange = (event) => {
+    setSliderValue(event.target.value);
+    const svg = d3.select("svg");
+    svg.selectAll(".line").remove();
+    svg.selectAll("circle").remove();
+  };
+
+  const calculateSliderValues = () => {
+    const increment = (maxX - minX) / 4;
+    const values = [minX];
+
+    for (let i = 1; i < 4; i++) {
+      values.push((minX + increment * i).toFixed(2));
+    }
+
+    values.push(maxX);
+    return values;
+  };
+
+  return (
+    <div>
+      <label htmlFor="slider">Slider Label</label>
+      <input
+        type="range"
+        min="0"
+        max="100"
+        value={sliderValue}
+        onChange={handleSliderChange}
+        style={{ width: "400px" }}
+      />
+      <div
+        style={{
+          display: "flex",
+          justifyContent: "space-between",
+          padding: "0px ",
+        }}
+      >
+        {calculateSliderValues().map((value, index) => (
+          <span key={index}>{value}</span>
+        ))}
+      </div>
+      <svg />
+    </div>
+  );
 }
 
 export default LineChart;
--- economics-of-asylum-countries/src/pages/CountryPage.jsx
+++ economics-of-asylum-countries/src/pages/CountryPage.jsx
@@ -7,10 +7,12 @@ const CountryPage = () => {
 
   return (
     <div className="grid grid-cols-2 grid-rows-2">
-        <div>1 </div>
-        <div>2 </div>
-        <div>3 </div>
-        <div>4 </div>
+      <div>
+        <LineChart />
+      </div>
+      <div>2 </div>
+      <div>3 </div>
+      <div>4 </div>
     </div>
   );
 };

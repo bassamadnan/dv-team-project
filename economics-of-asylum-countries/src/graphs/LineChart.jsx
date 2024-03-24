57c313aa52e01e4fef12ee39823e9b3589bcb66f
Bipasha <bipashagarg06@gmail.com>
2024-03-25 01:12:51 +0530

    line chart done for each country...removed slider

--- economics-of-asylum-countries/src/graphs/LineChart.jsx
+++ economics-of-asylum-countries/src/graphs/LineChart.jsx
@@ -1,176 +1,78 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useRef } from "react";
 import * as d3 from "d3";
+import { countryState } from "../context/CountryProvider";
+import { lur_data } from "../utils/data_parser";
 
-function LineChart() {
-  const [sliderValue, setSliderValue] = useState(0);
-  const [minX, setMinX] = useState(0);
-  const [maxX, setMaxX] = useState(0);
+const LineChart = () => {
+  const { ID } = countryState();
+  const ref = useRef();
+  const currData = lur_data[ID];
 
   useEffect(() => {
-    const dataset1 = [
-      [0, 1],
-      [12, 20],
-      [24, 36],
-      [32, 50],
-      [40, 70],
-      [50, 100],
-      [55, 106],
-      [65, 123],
-      [73, 130],
-      [78, 134],
-      [83, 136],
-      [89, 138],
-      [100, 140],
-    ];
+    if (!currData) return;
 
-    var xValues = dataset1.map((d) => d[0]);
-    const minXValue = d3.min(xValues);
-    const maxXValue = d3.max(xValues);
-    setMinX(minXValue);
-    setMaxX(maxXValue);
+    const filteredData = currData.present.map((year) => ({
+      year: +year,
+      value: parseFloat(currData[year]),
+    }));
 
-    var filteredDataset = dataset1.filter((d) => d[0] <= sliderValue);
-
-    const margin = { top: 50, right: 50, bottom: 100, left: 50 };
+    const margin = { top: 50, right: 50, bottom: 50, left: 50 };
     const width = 500 - margin.left - margin.right;
     const height = 400 - margin.top - margin.bottom;
 
-    const xScale = d3.scaleLinear().domain([0, 100]).range([0, width]);
-    const yScale = d3.scaleLinear().domain([0, 200]).range([height, 0]);
+    const xScale = d3
+      .scaleLinear()
+      .domain(d3.extent(filteredData, (d) => d.year))
+      .range([0, width]);
 
-    const svg = d3
-      .select("svg")
+    const yScale = d3
+      .scaleLinear()
+      .domain([0, d3.max(filteredData, (d) => d.value)])
+      .range([height, 0]);
+
+    const svg = d3.select(ref.current);
+
+    svg.selectAll("*").remove();
+
+    svg
       .attr("width", width + margin.left + margin.right)
-      .attr("height", height + margin.top + margin.bottom)
+      .attr("height", height + margin.top + margin.bottom);
+
+    const g = svg
       .append("g")
-      .attr("transform", `translate(${margin.left}, ${margin.top})`);
-
-    // Remove previous lines and circles
-    svg.selectAll(".line").remove();
-    svg.selectAll("circle").remove();
-
-    svg
-      .append("text")
-      .attr("x", width / 2)
-      .attr("y", 0 - margin.top / 2)
-      .attr("text-anchor", "middle")
-      .style("font-family", "Helvetica")
-      .style("font-size", 20)
-      .text("Line Chart");
-
-    svg
-      .append("text")
-      .attr("x", width / 2)
-      .attr("y", height + 40)
-      .attr("text-anchor", "middle")
-      .style("font-family", "Helvetica")
-      .style("font-size", 12)
-      .text("Year");
-
-    svg
-      .append("text")
-      .attr("transform", "rotate(-90)")
-      .attr("y", 0 - margin.left)
-      .attr("x", 0 - height / 2)
-      .attr("dy", "1em")
-      .style("text-anchor", "middle")
-      .style("font-family", "Helvetica")
-      .style("font-size", 12)
-      .text("GDP");
-
-    svg
-      .append("g")
-      .attr("transform", `translate(0, ${height})`)
-      .call(d3.axisBottom(xScale));
-
-    svg.append("g").call(d3.axisLeft(yScale));
-
-    svg
-      .selectAll("circle")
-      .data(filteredDataset)
-      .enter()
-      .append("circle")
-      .attr("cx", (d) => xScale(d[0]))
-      .attr("cy", (d) => yScale(d[1]))
-      .attr("r", 3)
-      .style("fill", "#CC0000");
+      .attr("transform", `translate(${margin.left},${margin.top})`);
 
     const line = d3
       .line()
-      .x((d) => xScale(d[0]))
-      .y((d) => yScale(d[1]))
-      .curve(d3.curveMonotoneX);
+      .x((d) => xScale(d.year))
+      .y((d) => yScale(d.value));
 
-    svg
-      .append("path")
-      .datum(filteredDataset)
-      .attr("class", "line")
-      .attr("d", line)
-      .style("fill", "none")
-      .style("stroke", "#CC0000")
-      .style("stroke-width", "2");
+    g.append("path")
+      .datum(filteredData)
+      .attr("fill", "none")
+      .attr("stroke", "steelblue")
+      .attr("stroke-linejoin", "round")
+      .attr("stroke-linecap", "round")
+      .attr("stroke-width", 2)
+      .attr("d", line);
 
-    // Add legend
-    svg
+    g.selectAll("circle")
+      .data(filteredData)
+      .enter()
       .append("circle")
-      .attr("cx", width - 100)
-      .attr("cy", 10)
-      .attr("r", 3)
-      .style("fill", "#CC0000");
+      .attr("cx", (d) => xScale(d.year))
+      .attr("cy", (d) => yScale(d.value))
+      .attr("r", 4)
+      .attr("fill", "steelblue");
 
-    svg
-      .append("text")
-      .attr("x", width - 90)
-      .attr("y", 10)
-      .text("Data")
-      .style("font-size", "12px")
-      .attr("alignment-baseline", "middle");
-  }, [sliderValue]);
+    g.append("g")
+      .attr("transform", `translate(0,${height})`)
+      .call(d3.axisBottom(xScale));
 
-  const handleSliderChange = (event) => {
-    setSliderValue(event.target.value);
-    const svg = d3.select("svg");
-    svg.selectAll(".line").remove();
-    svg.selectAll("circle").remove();
-  };
+    g.append("g").call(d3.axisLeft(yScale));
+  }, [currData]);
 
-  const calculateSliderValues = () => {
-    const increment = (maxX - minX) / 4;
-    const values = [minX];
-
-    for (let i = 1; i < 4; i++) {
-      values.push((minX + increment * i).toFixed(2));
-    }
-
-    values.push(maxX);
-    return values;
-  };
-
-  return (
-    <div>
-      <label htmlFor="slider">Slider Label</label>
-      <input
-        type="range"
-        min="0"
-        max="100"
-        value={sliderValue}
-        onChange={handleSliderChange}
-        style={{ width: "400px" }}
-      />
-      <div
-        style={{
-          display: "flex",
-          justifyContent: "space-between",
-          padding: "0px ",
-        }}
-      >
-        {calculateSliderValues().map((value, index) => (
-          <span key={index}>{value}</span>
-        ))}
-      </div>
-      <svg />
-    </div>
-  );
-}
+  return <svg ref={ref} />;
+};
 
 export default LineChart;
--- economics-of-asylum-countries/src/pages/CountryPage.jsx
+++ economics-of-asylum-countries/src/pages/CountryPage.jsx
@@ -9,11 +9,13 @@ const CountryPage = () => {
   const { ID } = countryState();
   // console.log(ID, numeric_to_alpha3[ID]);
   return (
-    <div className="grid grid-cols-2 grid-rows-2">
+    <div className="flex flex-col">
       <div>
         <Map  countryCode={numeric_to_alpha3[ID]}/>
       </div>
-      <div>2 </div>
+      <div>
+        <LineChart />
+      </div>
       <div>3 </div>
       <div>4 </div>
     </div>

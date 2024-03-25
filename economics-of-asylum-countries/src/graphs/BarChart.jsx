591b4a9d6730af970d02372d10dc1d89864edfcd
bassamadnan <bassamadnan2012@gmail.com>
2024-03-25 22:36:27 +0530

    style bar chart

--- economics-of-asylum-countries/src/graphs/BarChart.jsx
+++ economics-of-asylum-countries/src/graphs/BarChart.jsx
@@ -2,25 +2,27 @@ import React, { useEffect, useRef } from "react";
 import * as d3 from "d3";
 import { countryState } from "../context/CountryProvider";
 import { gdp_data } from "../utils/data_parser";
+// https://kamibrumi.medium.com/getting-started-with-react-d3-js-d86ccea05f08 <- started code
+
 
 const BarChart = () => {
   const { ID } = countryState();
   const ref = useRef();
   const curr_data = gdp_data[ID];
   const data = [];
-  
+
   for (const year of curr_data.present) {
-    const value = parseFloat(curr_data[year].replace(/,/g, ''));
+    const value = parseFloat(curr_data[year].replace(/,/g, ""));
     data.push({ Year: year, Value: value });
   }
 
-  console.log(data);
+  // console.log(data);
 
   useEffect(() => {
     // set the dimensions and margins of the graph
     const margin = { top: 30, right: 30, bottom: 70, left: 60 },
-      width = 460 - margin.left - margin.right,
-      height = 400 - margin.top - margin.bottom;
+      width = 800 - margin.left - margin.right,
+      height = 600 - margin.top - margin.bottom;
 
     // append the svg object to the body of the page
     const svg = d3
@@ -46,22 +48,61 @@ const BarChart = () => {
       .style("text-anchor", "end");
 
     // Add Y axis
-    const y = d3.scaleLinear().domain([0, d3.max(data, (d) => d.Value)]).range([height, 0]);
+    const y = d3
+      .scaleLinear()
+      .domain([0, d3.max(data, (d) => d.Value)])
+      .range([height, 0]);
     svg.append("g").call(d3.axisLeft(y));
 
     // Bars
-    svg
+    const bars = svg
       .selectAll("mybar")
       .data(data)
       .join("rect")
       .attr("x", (d) => x(d.Year))
-      .attr("y", (d) => y(d.Value))
+      .attr("y", height)
       .attr("width", x.bandwidth())
-      .attr("height", (d) => height - y(d.Value))
-      .attr("fill", "#5f0f40");
+      .attr("height", 0);
+
+    // Animation
+    bars
+      .transition()
+      .duration(800)
+      .attr("y", (d) => y(d.Value))
+      .attr("height", (d) => height - y(d.Value));
+
+    // Tooltip
+    const tooltip = d3
+      .select("body")
+      .append("div")
+      .style("position", "absolute")
+      .style("z-index", "10")
+      .style("visibility", "hidden")
+      .style("background-color", "white")
+      .style("border", "1px solid black")
+      .style("padding", "5px");
+
+    bars
+      .on("mouseover", function (event, d) {
+        tooltip
+          .style("visibility", "visible")
+          .html(`Year: ${d.Year}<br>Value: ${d.Value}`);
+      })
+      .on("mousemove", function (event) {
+        tooltip
+          .style("top", event.pageY - 10 + "px")
+          .style("left", event.pageX + 10 + "px");
+      })
+      .on("mouseout", function () {
+        tooltip.style("visibility", "hidden");
+      });
   }, []);
 
-  return <svg width={460} height={400} id="barchart" ref={ref} />;
+  return (
+    <div style={{"background": "steelblue", color:"black"}}>
+      <svg width={800} height={600} id="barchart" ref={ref} />
+    </div>
+  );
 };
 
 export default BarChart;
